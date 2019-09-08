9e3f7a2d012074874607a5d7d90646b0e7d96960
Juan Pablo Armenta <juanpabload98@gmail.com>
2019-09-08 17:49:47 -0500

    ya acomodado con tries

--- src/PrincipalCliente.java
+++ src/PrincipalCliente.java
@@ -5,11 +5,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PrincipalCliente {
 
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("127.0.0.1", 2500);
+        iniciarCliente(args[0]);
+       /* Socket socket = new Socket("127.0.0.1",Integer.parseInt(args[0]));
         PrintWriter escritor = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String datos;
@@ -18,14 +21,61 @@ public class PrincipalCliente {
         Scanner scanner = new Scanner(System.in);
         datos = scanner.nextLine();
         escritor.println(datos);
-        while (true) {
+        //while (true) {
             datosEntrada = lector.readLine();
             if ((datosEntrada.equals(""))) {
                 System.exit(0);
             }
             System.out.println(datosEntrada);
-        }
+       // }*/
 
+    }
+    public static void iniciarCliente(String puerto){
+        try {
+            CrearCliente(puerto);
+        } catch (Exception e) {
+           System.out.println("Error al iniciar cliente "+e);
+        }
+    }
+    public static void CrearCliente(String puerto){
+        try {
+            Socket socket = new Socket("127.0.0.1",Integer.parseInt(puerto));
+            ConfigurarCliente(socket);
+        } catch (IOException ex) {
+             System.out.println("Error al crear socket "+ex);
+        }
+    }
+    public static void ConfigurarCliente(Socket socket){
+        PrintWriter escritor = null;
+        try {
+            escritor = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException ex) {
+            System.out.println("Error al crear escritor "+ex);
+        }
+        BufferedReader lector = null;
+        try {
+            lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException ex) {
+            System.out.println("Error al crear lector "+ex);
+        }
+        String datos;
+        String datosEntrada = "";
+        String todo = "";
+        Scanner scanner = new Scanner(System.in);
+        datos = scanner.nextLine();
+        escritor.println(datos);
+        while (true) {
+        try {
+            //while (true) {
+            datosEntrada = lector.readLine();
+        } catch (IOException ex) {
+            System.out.println("Error al leer respuesta "+ex);
+        }
+            if ((datosEntrada.equals(""))) {
+                System.exit(0);
+            }
+            System.out.println(datosEntrada);
+         }
     }
 
 }
